dc32237644b9162a3aebfc24df9929441d1916f9
aMoltu <aleksandermoltu@gmail.com>
2025-02-28 20:18:49 +0100

    feat: add hand of cards with various checks

---
+++ src/main/java/edu/ntnu/idi/idatt/HandOfCards.java
@@ -0,0 +1,32 @@
+package edu.ntnu.idi.idatt;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class HandOfCards extends ArrayList<PlayingCard> {
+  private final char[] suit = {'S', 'H', 'D', 'C'};
+
+  public int getSum(){
+    return this.stream().mapToInt(PlayingCard::getFace).sum();
+  }
+
+  public HandOfCards getHearts() {
+    return this.stream()
+        .filter(card -> card.getSuit() == 'H')
+        .collect(Collectors.toCollection(HandOfCards::new));
+  }
+
+  public boolean hasQueenOfSpades() {
+    PlayingCard queenOfSpades = new PlayingCard('S', 12);
+    return this.contains(queenOfSpades);
+  }
+
+  public boolean isFlush() {
+    for (char s : suit) {
+      if (this.stream().filter(card -> card.getSuit() == s).count() == 5){
+        return true;
+      }
+    }
+    return false;
+  }
+}
